fbecf17368544f21be7d24fe1596617fb74dedea
malbernaz <albernazmiguel@gmail.com>
2017-07-14 12:21:23 -0300

    remove unuseful certs folder & clean deps

--- src/lib/withStyles.js
+++ src/lib/withStyles.js
@@ -1,26 +1,16 @@
 import { h, Component } from "preact";
-import hoistStatics from "hoist-non-react-statics";
 
 export default function withStyles(...styles) {
-  return function wrapWithStyles(WrappedComponent) {
-    const displayName = WrappedComponent.displayName || WrappedComponent.name || "Component";
-
+  return WrappedComponent =>
     class WithStyles extends Component {
-      static displayName = `WithStyles(${displayName})`;
-
       componentWillMount() {
         this.removeCss = this.context.insertCss.apply(undefined, styles);
       }
-
       componentWillUnmount() {
         setTimeout(this.removeCss, 0);
       }
-
-      render() {
-        return <WrappedComponent {...this.props} />;
+      render(props) {
+        return <WrappedComponent {...props} />;
       }
-    }
-
-    return hoistStatics(WithStyles, WrappedComponent);
-  };
+    };
 }
